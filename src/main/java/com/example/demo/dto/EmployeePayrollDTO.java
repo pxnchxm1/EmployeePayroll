89ee0ee7d92b4107f832f3273b68ceae7a739354
pxnchxm1 <you@example.companchamipkumar1995@gmail.com>
2025-05-25 20:40:19 +0530

    added:uc10_name_validation

--- src/main/java/com/example/demo/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/demo/dto/EmployeePayrollDTO.java
@@ -1,5 +1,8 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmployeePayrollDTO {
-	private long id;
 	
+	private long id;
+	@NotNull(message = "Employee name cannot be null")
+	@Pattern(regexp = "^[A-Z][a-zA-Z\\s]{2,}$", message = "Employee name is invalid")
 	private String name;
+
+	@Min(value = 500, message = "Salary should be more than 500")
 	private double salary;
 
 }
